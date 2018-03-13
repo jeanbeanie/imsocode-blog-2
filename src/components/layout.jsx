0a21f712ba1816f5a0924af15af1694dc6f70d01
jean <msjeanneramos@gmail.com>
2018-03-13 14:08:22 -0400

    changed Layout component to match the layout of the original imsocode website

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -9,21 +9,35 @@ type Props = {
   children: React.Node,
 };
 
+   // <nav className="navbar justify-content-end">
+    //  <Link to="/" className="nav-link">HOME</Link>
+   // </nav>
+const imageSource = 'https://pbs.twimg.com/profile_images/424484505915621376/EOwsjaMZ.png';
 const Layout = (props: Props) => (
   <div className="container" id="layout-container">
+    <div className="row">
 
-    <nav className="navbar justify-content-end">
-      <Link to="/" className="nav-link">HOME</Link>
-    </nav>
+      <div className="jumbotron col-md-3">
 
-    <div className="jumbotron">
-      {props.children}
+        <div id="header">
+          <Link to="/" className="nav-link">
+            <h2>{'im{so}code'}</h2>
+            <hr />
+            <img src={imageSource} className="rounded-circle" />
+          </Link>
+
+          <p><Link className="nav-link" to="/">Projects</Link></p>
+          <p><Link className="nav-link" to="/">Blog</Link></p>
+          <p><Link className="nav-link" to="/">Contact</Link></p>
+        </div>
+
+      </div>
+
+      <div className="col-md-9">
+        {props.children}
+      </div>
     </div>
   </div>
 );
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-};
 
 export default Layout;
