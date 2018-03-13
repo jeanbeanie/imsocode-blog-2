90e3ddc0f3a81af0572eb6e11ca2cd1b0e0653af
jean <msjeanneramos@gmail.com>
2018-03-13 17:34:47 -0400

    added some css ids/classes to elements and added a footer

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,40 +2,39 @@
 /* @flow */
 
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 type Props = {
   children: React.Node,
 };
 
-   // <nav className="navbar justify-content-end">
-    //  <Link to="/" className="nav-link">HOME</Link>
-   // </nav>
 const imageSource = 'https://pbs.twimg.com/profile_images/424484505915621376/EOwsjaMZ.png';
 const Layout = (props: Props) => (
   <div className="container" id="layout-container">
     <div className="row">
 
-      <div className="jumbotron col-md-3">
-
+      <div className="jumbotron col-md-3" id="left-column">
         <div id="header">
           <Link to="/" className="nav-link">
             <h2>{'im{so}code'}</h2>
             <hr />
-            <img src={imageSource} className="rounded-circle" />
+            <img src={imageSource} alt="" className="rounded-circle" id="logo-pic"/>
           </Link>
 
           <p><Link className="nav-link" to="/">Projects</Link></p>
           <p><Link className="nav-link" to="/">Blog</Link></p>
           <p><Link className="nav-link" to="/">Contact</Link></p>
         </div>
-
       </div>
 
-      <div className="col-md-9">
+      <div className="col-md-9" id="right-column">
         {props.children}
+        <footer id="footer">
+          <hr />
+          <p>&copy; 2017 ImSoCode.com</p>
+        </footer>
       </div>
+
     </div>
   </div>
 );
