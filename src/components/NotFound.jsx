e5ba0355109548a94a53b1deae1da706695a50ad
jean <msjeanneramos@gmail.com>
2018-03-15 15:04:03 -0400

    uses passed in prop title instead of metaTitle

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -5,13 +5,13 @@ import React from 'react';
 import Status from './Status';
 
 type Props = {
-  metaTitle: string,
+  title: string,
 };
 
 const NotFound = (props: Props) => (
   <Status code={404}>
     <div>
-      <h1>{props.metaTitle}</h1>
+      <h1>{props.title}</h1>
     </div>
   </Status>
 );
