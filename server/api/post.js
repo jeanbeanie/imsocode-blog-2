7de6e66ddf07ded444a590bb8694bd30fb4ba393
jean <msjeanneramos@gmail.com>
2018-03-15 15:11:53 -0400

    moved api functions file post.js into the server/api directory

--- server/api/post.js
+++ server/api/post.js
@@ -1,12 +1,15 @@
-/* src/api/post.js */
+/* server/api/post.js */
 const { MongoClient } = require('mongodb');
 const assert = require('assert');
-const url = require('../config').MONGODB_URL;
+const url = require('../../src/config').MONGODB_URL;
 
 module.exports = {
   getPosts(callback) {
-    MongoClient.connect(url, (err0, db) => {
+    MongoClient.connect(url, (err0, database) => {
+      if(err0){console.log('err0',err0)}
+      const db = database.db('Zootgamer');
       db.collection('post', (err1, collection) => {
+        if(err1){console.log('err1',err1)}
         collection.find().toArray((err2, list) => {
           callback(list);
         });
