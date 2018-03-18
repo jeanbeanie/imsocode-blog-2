511b910f158a988eeb1c090c7ee2192bf1b63580
jean <msjeanneramos@gmail.com>
2018-03-18 12:23:44 -0400

    made PostsContainer into class component, loading posts when first mounted through the passed in loadInitialData prop function

--- src/components/posts/PostsContainer.jsx
+++ src/components/posts/PostsContainer.jsx
@@ -4,22 +4,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-type Props = {
-  posts: [],
-}
+class PostsContainer extends React.Component <{posts?: []}, {posts?: []}> {
+  constructor(props: {posts?: []}) {
+    super(props);
+    this.state = {
+      posts: this.props.posts || [],
+    };
+  }
 
-const PostsContainer = (props: Props) => (
-  <div>
-    {
-      props.posts.map((post, index) => (
-        <div className="post-index-item" key={index}>
-          <Link to={`/blog/${post.slug}`}><h4> {post.title} </h4></Link>
-          <p> {post.summary} <Link to={`/blog/${post.slug}`}>+Read more</Link></p>
-          <p className="post-published-date"> {post.publishedDate} </p>
-        </div>
-      ))
-    }
-  </div>
-);
+  componentDidMount() {
+    this.loadInitialData();
+  }
+
+  loadInitialData() {
+    const promises = [];
+    promises.push(this.props.loadInitialData())
+    Promise.all(promises).then((data) => {
+      this.setState(data[0]);
+    }).catch(err => console.log('Error loading data in PostsContainer', err));
+  }
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <div>
+        {
+          posts.map((post, index) => (
+            <div className="post-index-item" key={index}>
+              <Link to={`/blog/${post.slug}`}><h4> {post.title} </h4></Link>
+              <p> {post.summary} <Link to={`/blog/${post.slug}`}>+Read more</Link></p>
+              <p className="post-published-date"> {post.publishedDate} </p>
+            </div>
+          ))
+        }
+      </div>
+    );
+  }
+}
+PostsContainer.defaultProps = {
+  posts: [],
+};
 
 export default PostsContainer;
