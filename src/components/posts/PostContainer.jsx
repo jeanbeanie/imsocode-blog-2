89372e8d1aa0a29d4bc9724d0a832a468381233c
jean <msjeanneramos@gmail.com>
2018-03-18 12:23:09 -0400

    made PostContainer into a class component, am now loading data from the passed in loadInitialData prop function, and properly rendering on the client side

--- src/components/posts/PostContainer.jsx
+++ src/components/posts/PostContainer.jsx
@@ -2,23 +2,44 @@
 /* @flow */
 
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-type Props = {
+class PostContainer extends React.Component <{post?: {}}> {
+  constructor(props: {post?: {}}) {
+    super(props);
+    this.state = {
+      post: this.props.post || {},
+    };
+  }
+
+  componentDidMount() {
+    this.loadInitialData();
+  }
+
+  loadInitialData() {
+    const promises = [];
+    const url = window.document.location.pathname;
+    promises.push(this.props.loadInitialData(url))
+    Promise.all(promises).then((data) => {
+      this.setState(data[0]);
+    }).catch(err => console.log('Error loading data in PostContainer', err));
+  }
+
+  render() {
+    const { post } = this.state;
+    return (
+      <div>
+        <h2 className="post-title">{post.title}</h2>
+        <div dangerouslySetInnerHTML={{ __html: post.subject }} />
+        <p className="font-italic"> Published {post.publishedDate} under <label> category : {post.category}</label></p>
+        <hr />
+        <p><strong>Connect and send feedback about this article, <a href="">follow me on Twitter!</a></strong></p>
+      </div>
+    );
+  }
+};
+
+PostContainer.defaultProps = {
   post: {},
-}
-
-const PostContainer = (props: Props) => {
-  const { post } = props;
-  return (
-    <div>
-      <h2 className="post-title">{post.title}</h2>
-      <div dangerouslySetInnerHTML={{ __html: post.subject }} />
-      <p className="font-italic"> Published {post.publishedDate} under <label> category : {post.category}</label></p>
-      <hr />
-      <p><strong>Connect and send feedback about this article, <a href="">follow me on Twitter!</a></strong></p>
-    </div>
-  );
-}
+};
 
 export default PostContainer;
